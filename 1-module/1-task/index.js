6c86585068b3914914fa1ed308d688a3ae9325be
damir-art <damir-art@ya.ru>
2023-07-06 11:57:49 +0300

    1-module 1-5 task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let fact = 1;
+
+  if(n === 0 || n === 1) {
+    return 1;
+  }
+  for (let i = 2; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
 }
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,11 @@
 function checkSpam(str) {
-  // ваш код...
+  // превращаем строку в нижний регистр
+  // проверяем на соответсвие '1xBet' или 'XXX'
+  if ( str.toLowerCase().indexOf("1xbet") > -1 ) {
+    return true;
+  } else if ( str.toLowerCase().indexOf("xxx") > -1 ) {
+    return true;
+  } else {
+    return false;
+  }
 }
