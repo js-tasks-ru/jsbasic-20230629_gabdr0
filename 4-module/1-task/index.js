12d8c771ea34fca2e1fdb2ce4cc809f832182557
damir-art <damir-art@ya.ru>
2023-07-25 13:41:00 +0300

    4-module 1-3st task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const newFriends = friends.map( obj => `${obj.firstName} ${obj.lastName}` );
+  const ul         = document.createElement("ul");
+
+  for (const item of newFriends) {
+    const li       = document.createElement("li");
+    li.textContent = item;
+    ul.append(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  for (const tr of table.tBodies[0].rows) {
+    if( tr.cells[3].dataset.available === "true" ) {
+      tr.classList.add( "available" );
+    } else if ( tr.cells[3].dataset.available === "false" ) {
+      tr.classList.add( "unavailable" );
+    } else if ( tr.cells[3].dataset.available === undefined ) {
+      tr.setAttribute( "hidden", "hidden" );
+    }
+
+    if( tr.cells[2].textContent === "m" ) {
+      tr.classList.add( "male" );
+    } else {
+      tr.classList.add( "female" );
+    }
+
+    if( Number(tr.cells[1].textContent) < 18 ) {
+      tr.style.textDecoration = "line-through";
+    }
+  }
 }
