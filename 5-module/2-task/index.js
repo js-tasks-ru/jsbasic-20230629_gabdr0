3a66aa8f35fd7e51f0bac798bcbcdd8af32eafae
damir-art <damir-art@ya.ru>
2023-07-31 08:39:29 +0300

    5-module 1-3st task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  const toggleTextButton = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+
+  toggleTextButton.addEventListener( "click", function(evt) {
+    if (text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  });
 }
